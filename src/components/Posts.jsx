fc3e68f97a032a01e03111e36381957db485f14d
음지훈 <jhmc5268@naver.com>
2023-02-19 03:04:28 +0900

    feat: 상세페이지정보가져옴

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+function Posts(item) {
+
+  // 상세페이지 만들기 위해 
+  const navigate = useNavigate();
+
+  // usenavigate를 이용해서 item:id로 이동하고 
+  // 이동하면서 item 값을 같이 보냄
+  const onClickDeatilPage = () => {
+    navigate(`/${item.item.id}`, {
+      state: item
+    })
+  }
+
+
+  // console.log(item.item)
+  return (
+
+    <PostArea onClick={onClickDeatilPage}>
+      <div><WrapImg src={item.item.imageFile} /></div>
+
+      <div>
+        <WrapH3>{item.item.title}</WrapH3>
+        <WarpP>{item.item.body} </WarpP>
+        <WarpSpan>23.02.19</WarpSpan>
+
+      </div>
+
+
+    </PostArea>
+
+  )
+}
+
+export default Posts
+
 
 const PostArea = styled.div`
   display: flex;
@@ -9,6 +49,7 @@ const PostArea = styled.div`
   height: 12.5rem;
   border-radius: 1.25rem;
   background: white;
+  margin-bottom: 3.125rem;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
   
 `;
@@ -42,27 +83,3 @@ const WarpSpan = styled.p`
   border: 1px solid red;
 
 `
-
-// 여기에서 데이터 받아서 바인딩
-
-function Posts(item) {
-  // console.log(item.item)
-  return (
-
-    <PostArea>
-      <div><WrapImg src={item.item.imageFile} /></div>
-
-      <div>
-        <WrapH3>{item.item.title}</WrapH3>
-        <WarpP>{item.item.body} </WarpP>
-        <WarpSpan>23.02.19</WarpSpan>
-
-      </div>
-
-
-    </PostArea>
-
-  )
-}
-
-export default Posts
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import styled from "styled-components";
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Detail() {
+    // Todo에서 상세보기를 누르면 그거에 객체를 보내준다 
+    const { state } = useLocation();
+    console.log(state)
+    // navigate훅을 이용해서 돌아가기 구현 
+    const navigate = useNavigate();
     return (
         <div>Detail</div>
     )
